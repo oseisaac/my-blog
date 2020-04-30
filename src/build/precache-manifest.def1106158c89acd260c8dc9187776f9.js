075aad4edfbb02886592fc8303347e86603f3b5b
Isaac <47258925+oseisaac@users.noreply.github.com>
2020-04-30 00:32:47 -0400

    inital commit

---
+++ src/build/precache-manifest.def1106158c89acd260c8dc9187776f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737e5acadd21d5900cb121ca976286c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da4e2f5a5925ac5cc3f",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "3c067b180a8e2458b995",
+    "url": "/static/js/2.417d031b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.417d031b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3da4e2f5a5925ac5cc3f",
+    "url": "/static/js/main.e29586ea.chunk.js"
+  },
+  {
+    "revision": "4f8b734a7e80109b6f7e",
+    "url": "/static/js/runtime-main.10a268e8.js"
+  }
+]);
